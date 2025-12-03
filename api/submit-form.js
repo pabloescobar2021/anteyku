6df3d82d48899fa3ea3a099f301a927277692ed2
pabloescobar2021 <mishutinpavlusha123@gmail.com>
2025-12-03 19:02:39 +0300

    Updates

--- api/submit-form.js
+++
@@ -1,9 +0,0 @@
-export async function POST ({request}) {
-    const data = await request.json();
-    console.log('Получено', data);
-
-    return new Response(JSON.stringify({ succes: true}),{
-        status: 200,
-        headers: { 'Content-Type': 'application/json' }
-    })
-}
